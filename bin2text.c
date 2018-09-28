28bd051d391c7d687db308d6e7d7dbdf2467fef0
Connor O'Hara <chuckhead7@gmail.com>
2018-09-28 13:16:39 -0400

    Finished bin2text

--- bin2text.c
+++ bin2text.c
@@ -15,8 +15,27 @@ int main(int argc, char** argv) {
 
 	toRead = fopen(argv[1], "r"); 
 	toWrite = fopen(argv[2], "wb");
+	
+	while (!feof(toRead)) {
+		unsigned short uid;
+		fread(&uid, sizeof(uid), 1, toRead);
+		fprintf(toWrite, "%hu", uid);
+		fprintf(toWrite, "%c", tab);
+		
+		unsigned short iid;
+		fread(&iid, sizeof(iid), 1, toRead);
+		fprintf(toWrite, "%hu", iid);
+		fprintf(toWrite, "%c", tab);
 
-	unsigned short uid;
-	fread(&uid, sizeof(uid), 1, toRead);
-	fprintf(toWrite, "%hu", uid);
+		unsigned char rating;
+		fread(&rating, sizeof(rating), 1, toRead);
+		//fgets(&rating, sizeof(rating), toRead);
+		fprintf(toWrite, "%u", rating);
+		fprintf(toWrite, "%c", tab);
+		
+		unsigned long timestamp;
+		fread(&timestamp, sizeof(timestamp), 1, toRead);
+		fprintf(toWrite, "%lu", timestamp);
+		fprintf(toWrite, "%c", '\n');
+	}
 }
